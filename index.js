863f4e65684e40db6fceda1e2c0c07b3c236e849
Mushtaq-MK <Mushtaq-MK@ulster.ac.uk>
2026-01-10 03:08:55 +0300

    yml update 3

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Server is running...' })
+  res.json({ status: 'ok', message: 'Server is running..!!!!' })
 })
 
 app.listen(PORT, () => {
